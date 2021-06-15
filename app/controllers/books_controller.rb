0d7ff3bc1bb76f7120e5399b124d77a58f998467
Ryodai-15 <gon.4629.1208@gmail.com>
2021-06-15 01:47:09 +0000

    [finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,20 +25,25 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
  private
   def book_params
-    params.permit(:title,:body)
+    params.require(:book).permit(:title,:body)
   end
 
 end
